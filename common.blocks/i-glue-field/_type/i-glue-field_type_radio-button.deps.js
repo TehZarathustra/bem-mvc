473c278e30635442c5e5ff5ef4973fc67f159dc7
v-prusakov <v-prusakov@yandex-team.ru>
2017-10-17 18:22:26 +0300

    Добавил radio-button

---
+++ common.blocks/i-glue-field/_type/i-glue-field_type_radio-button.deps.js
@@ -0,0 +1,16 @@
+({
+    shouldDeps: [
+        {
+	        block: 'radio-button',
+	        mods: {
+	            size: 'm',
+	            theme: 'normal'
+	        },
+	        elems: [
+	            {
+	                elem: 'radio'
+	            }
+	        ]
+	    }
+    ]
+})
